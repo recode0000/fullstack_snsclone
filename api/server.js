bebe46cae67b9651cba19f650db232f77be0e254
Rikito Ikeda <rikito1036@icloud.com>
2023-06-04 10:15:51 +0900

    モジュール化

--- api/server.js
+++ api/server.js
@@ -1,33 +1,12 @@
 const express = require("express");
 const app = express();
+const authRoute = require("./routers/auth");
 const PORT = 8080;
-const bcrypt = require("bcrypt");
-
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+// Node.jsでenvファイルを使用できるようにする
+require("dotenv").config();
 
 app.use(express.json());
 
-// 新規ユーザー登録API
-app.post("/api/auth/register", async (req, res) => {
-  const { username, email, password } = req.body;
-
-  // bcryptでパスワードハッシュ化
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  return res.json({ user });
-});
-
-
-// ユーザーログインAPI
-
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
