da6f7d5118f0aeba90673eea5c78f79c49f2997e
Rikito Ikeda <rikito1036@icloud.com>
2023-06-04 15:06:07 +0900

    ロード時に最新10件取得するAPI

---
+++ api/routers/posts.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const jwt = require("jsonwebtoken"); // トークン発行
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// 投稿
+router.post("/post", async (req, res) => {
+  const { content } = req.body;
+
+  if(!content) {
+    return res.status(400).json({ message: '投稿内容がありません'});
+  }
+
+  try {
+    const newPost = await prisma.post.create({
+      data: {
+        content,
+        authorId: 1,
+      },
+    });
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'サーバーエラーです。'});
+  }
+});
+
+
+// 最新10件投稿取得
+router.get("/get_latest_post", async (req, res) => {
+
+  try {
+    const latestPosts = await prisma.post.findMany({
+      take: 10,
+      orderBy: { createdAt: "desc" }
+    });
+
+    return res.json(latestPosts)
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'サーバーエラーです。'});
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
+const postsRoute = require("./routers/posts");
 const cors = require("cors")
 require("dotenv").config(); // Node.jsでenvファイルを使用できるようにする
 
@@ -8,4 +9,5 @@ const PORT = 8080;
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
