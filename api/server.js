7341c3736ee52821a820e95cea3cd8c194f21a2e
Rikito Ikeda <rikito1036@icloud.com>
2023-06-04 11:27:36 +0900

    corライブラリ追加 同一オリジンポリシー回避

--- api/server.js
+++ api/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
+const cors = require("cors")
+require("dotenv").config(); // Node.jsでenvファイルを使用できるようにする
+
 const PORT = 8080;
-// Node.jsでenvファイルを使用できるようにする
-require("dotenv").config();
-
+app.use(cors());
 app.use(express.json());
-
 app.use("/api/auth", authRoute);
-
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
